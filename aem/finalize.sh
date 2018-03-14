cf201e66828a6b0f27a41e392b442b4a1174aa5e
Ty Esser <ty.esser@vml.com>
2018-03-13 23:45:16 -0500

    change how packages are installed (can now set a flag to skip)

--- aem/finalize.sh
+++ aem/finalize.sh
@@ -5,16 +5,18 @@ source aem-config.sh
 baseUrl=$BASE_URL
 password=$LOGIN_INFO
 
-#curl $baseUrl/etc/replication/agents.author/publish/jcr:content --user $password --data-urlencode "{'transportUri': 'http://publisher:4503/bin/receive?sling:authRequestLogin=1'}"
+curl $baseUrl/etc/replication/agents.author/publish/jcr:content --user $password --data-urlencode "{'transportUri': 'http://publisher:4503/bin/receive?sling:authRequestLogin=1'}"
 
 #echo "checking agent"
-#curl $baseUrl/etc/replication/agents.author/publish/jcr:content.json --user $password
+curl $baseUrl/etc/replication/agents.author/publish/jcr:content.json --user $password
 
 echo "Switching to Classic UI"
 sed -i 's/TOUCH/CLASSIC/' /aem/crx-quickstart/launchpad/config/com/day/cq/wcm/core/impl/AuthoringUIModeServiceImpl.config
 
 echo "if packages exist, install them..."
-for file in $PACKAGES_PATH
+if [ $INSTALL_PACKAGES == 1 ]; then
+for file in /aem/packages/*
 do
   curl --user $password -F file=@"$file" -F name="$file" -F force=true -F install=true $baseUrl/crx/packmgr/service.jsp
 done
+fi
