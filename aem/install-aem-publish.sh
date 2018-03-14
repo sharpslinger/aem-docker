8f89fdf1920c7f08375148e84e2c90ec47ded0be
Ty Esser <ty.esser@vml.com>
2018-03-14 17:41:48 -0500

    fixed the publisher so it actually starts as a publish instance

--- aem/install-aem-publish.sh
+++ aem/install-aem-publish.sh
@@ -3,15 +3,15 @@
 # pull in configuration values
 source aem-config.sh
 
-port=4502
-finalizescript="/aem/finalize-author.sh"
+port=4503
+finalizescript="/aem/finalize-publish.sh"
 
 # install everything
-java -jar $AEM_PATH -listener-port 25000 -r author nosample -p $port 2>&1 &
+java -jar $AEM_PATH -listener-port 25005 -r publish -p $port 2>&1 &
 aemProcessId=$!
 
 # listen for activity, so we can figure out when done
-nc -vklp 25000 > "install.log" 2>&1 &
+nc -vklp 25005 > "install.log" 2>&1 &
 currentProcessId=$!
 
 # wait (while listening); check every 60s
